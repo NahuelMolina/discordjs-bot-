84c74d689845f9b2e638dbb193f08c7531eb568b
Silvio Molina <kindermolina98@gmail.com>
2020-09-19 19:30:11 -0300

    first commit

---
+++ src/main.js
@@ -0,0 +1,43 @@
+const Discord = require('discord.js'); 
+
+const options = {
+    shardCount:1,
+    messageCacheMaxSize:200,
+    fetchAllMembers:false,
+    restTimeOffset:500,
+    restRequestTimeout:15000,
+    restSweepInterval:60,
+    retryLimit:1
+}
+
+const client = new Discord.Client(options);
+const clientUser = new Discord.ClientUser();
+
+const token = 'NzUyNjAxODg1OTk5MTA0MDc2.X1aBIA.x1kmmTMudGobQc4dnOB2EBSvd4c';
+const probablyResponses = ['Fuck you', 'fuck you'];
+
+client.on('ready', async (received) => {
+	await console.log(`Bot is ready as ${client.user.tag}`);
+    
+    if(received == 'undefined'){
+        await console.log(typeof received);
+        await console.log(`The data received is undefined`);
+    };
+});
+
+client.on('message', msg => {
+
+    if( msg.content == 'Hola'){
+		msg.channel.send(`Hello ${msg.author}!!`);
+    }else if(msg.content == 'Whats is my avatar'){
+        msg.channel.send(`That is: ${message.author.displayAvatarURL()}`);
+        
+    }else{
+        msg.channel.send(`Send something mesmerizing`);
+    }
+
+
+});
+	
+
+client.login(token);
